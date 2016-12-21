8a80ef0373acb52e2bef507a17690d0f7e5f384d
Marvin Elz <MarvinElz@gmx.de>
2016-12-21 12:38:40 +0100

    Added Dynamic Parameter Support

--- src/rc_delay.cpp
+++ src/rc_delay.cpp
@@ -1,4 +1,8 @@
 #include "ros/ros.h"
+
+#include <dynamic_reconfigure/server.h>
+#include <quadrotor_rc_delay/DelayTimeConfig.h>
+
 #include "sensor_msgs/Joy.h"
 #include "std_msgs/Float32.h" // nur Testzweck
 #include <vector>
@@ -6,44 +10,56 @@
 std::vector<sensor_msgs::Joy::Ptr> queue;
 ros::Duration delay_time(0.01); 
 
+/*
 void callback_set_delay_time( const std_msgs::Float32::Ptr& msg ){
    ROS_INFO( "neue Delay Time: %f", msg->data);
    delay_time = ros::Duration( msg->data );   
 }
+*/
 
-void callback_rc_signal( const sensor_msgs::Joy::Ptr& msg )
+void callbackSetDelayTime(quadrotor_rc_delay::DelayTimeConfig &config, uint32_t level) {	
+	delay_time = ros::Duration( config.delay_time );  
+	ROS_INFO( "neue Delay Time: %f", config.delay_time );	
+}
+
+void callbackRCSignal( const sensor_msgs::Joy::Ptr& msg )
 {  
-  msg->header.stamp += delay_time;
-  queue.push_back(msg);
-  ROS_INFO("Queue Size: %lu\n", queue.size() );
+	msg->header.stamp += delay_time;
+	queue.push_back(msg);
+	ROS_INFO("Queue Size: %lu\n", queue.size() );
 }
 
 int main( int argc, char **argv )
 {
-  ros::init(argc, argv, "rc_delay");
+	ros::init(argc, argv, "rc_delay");
 
-  ros::NodeHandle nh;
+	ros::NodeHandle nh;
 
-  ros::Subscriber sub_Signal = nh.subscribe( "joy", 10, callback_rc_signal);
-  ros::Subscriber sub_Delay_time = nh.subscribe( "/set_Delay_Time", 100, callback_set_delay_time );
-  ros::Publisher pub = nh.advertise<sensor_msgs::Joy>("rc_signal_delayed", 1000);
+	ros::Subscriber sub_Signal = nh.subscribe( "joy", 10, callbackRCSignal);
 
-  ros::Rate loop_rate(100);
+	dynamic_reconfigure::Server<quadrotor_rc_delay::DelayTimeConfig> server;
+	dynamic_reconfigure::Server<quadrotor_rc_delay::DelayTimeConfig>::CallbackType f;
+	f = boost::bind(&callbackSetDelayTime, _1, _2);
+	server.setCallback(f);
 
-  while(ros::ok())
-  { 
+  	ros::Publisher pub = nh.advertise<sensor_msgs::Joy>("rc_signal_delayed", 1000);
 
-    ros::Time now = ros::Time::now();
-    while( queue.size() > 0 && queue.front()->header.stamp < now )
-    {
-       pub.publish(queue.front());
-       queue.erase(queue.begin());
-    }
+  	ros::Rate loop_rate(100);
 
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+  	while(ros::ok())
+  	{ 
 
-  ros::spin();
-  return 0;
+    		ros::Time now = ros::Time::now();
+    		while( queue.size() > 0 && queue.front()->header.stamp < now )
+    		{
+       			pub.publish(queue.front());
+       			queue.erase(queue.begin());
+    		}
+
+    		ros::spinOnce();
+    		loop_rate.sleep();
+  	}
+
+  	ros::spin();
+  	return 0;
 }
